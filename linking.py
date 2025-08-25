ebb7e68f1e9a5e4bf0b551c5256a4c420494db33
Ondřej Smetana <smetana@ucl.cas.cz>
2025-08-25 09:44:42 +0200

    read from csv + success rate

--- linking.py
+++ linking.py
@@ -1,11 +1,25 @@
 from DwnKramerius import Periodical, load_periodical
+from Parse773 import parse_location, normalize, remove_leading_zeros
 import csv
+import logging
 
-frenstat = load_periodical('data/frenstat_object_0728114733.json')
+logging.basicConfig(level=logging.INFO)
 
+path_to_periodical = 'data/narodopis_revue_session.json'
+path_to_marc = 'data/marc_data/narodopis_marc.csv'
 
-with open('data/marc_data/frenstat_marc.csv') as f:
+perio = load_periodical(path_to_periodical)
+
+count, fail = 0, 0
+with open(path_to_marc) as f:
     reader = csv.DictReader(f, delimiter=';')
     for row in reader:
+        count += 1
         loc = row["location"].split(';')
-        print(loc)
+        for item in loc:
+            path = normalize(*parse_location(item))
+            l = perio.link(*path)
+            print(f"https://vufind.ucl.cas.cz/Record/{row['id']}", item, l)
+            if l is None:
+                fail += 1
+print(f'Success rate {1-fail/count:.1%}')
