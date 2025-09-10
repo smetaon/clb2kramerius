38214a731a5a0b312630498c94a20fc3a86f31b6
Ondřej Smetana <smetana@ucl.cas.cz>
2025-09-10 16:26:12 +0200

    simple test for diagnosing 773q

---
+++ test_Linker.py
@@ -0,0 +1,15 @@
+from Linker import Kram2CLB, ErrorCodes
+from DwnKramerius import load_periodical
+
+
+def test_diagnose_773q():
+    perio = load_periodical('test_data/frenstat_test.json')
+    linker = Kram2CLB(perio, 'test_data/frenstat_marc.csv')
+    linker.link()
+    linker._diagnose_773q()
+    expected = [ErrorCodes.MISSING_ISSUE,
+                ErrorCodes.MISSING_PAGE,
+                ErrorCodes.MISSING_ISSUE,
+                ErrorCodes.MISSING_MULTIPLE]
+    for f, err in zip(linker.return_fails(), expected):
+        assert f.error_code is err
