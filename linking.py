8dbdd67c374e29e9c557cdd3ee4ffacd7913cc73
Ondřej Smetana <smetana@ucl.cas.cz>
2025-09-11 12:18:58 +0200

    employ the Linker class

--- linking.py
+++ linking.py
@@ -1,25 +1,20 @@
-from DwnKramerius import Periodical, load_periodical
-from Parse773 import parse_location, normalize, remove_leading_zeros
-import csv
+from DwnKramerius import load_periodical
+from Linker import Kram2CLB
 import logging
 
-logging.basicConfig(level=logging.INFO)
+FORMAT = "[%(asctime)s %(funcName)s():]%(levelname)s: %(message)s"
+logging.basicConfig(format=FORMAT, level=logging.INFO)
 
-path_to_periodical = 'data/narodopis_revue_session.json'
-path_to_marc = 'data/marc_data/narodopis_marc.csv'
+path_to_periodical = 'data/debug/frenstat.json'
+path_to_marc = 'data/marc_data/frenstat_marc.csv'
 
 perio = load_periodical(path_to_periodical)
-
-count, fail = 0, 0
-with open(path_to_marc) as f:
-    reader = csv.DictReader(f, delimiter=';')
-    for row in reader:
-        count += 1
-        loc = row["location"].split(';')
-        for item in loc:
-            path = normalize(*parse_location(item))
-            l = perio.link(*path)
-            print(f"https://vufind.ucl.cas.cz/Record/{row['id']}", item, l)
-            if l is None:
-                fail += 1
-print(f'Success rate {1-fail/count:.1%}')
+linker = Kram2CLB(perio, path_to_marc)
+linker.link()
+succ_rate = linker.success_rate()
+print(f'Succes rate {succ_rate:.1%}')
+linker.diagnose_fails()
+linker.fix_errors()
+linker.link()
+succ_rate = linker.success_rate()
+print(f'Succes rate {succ_rate:.1%}')
